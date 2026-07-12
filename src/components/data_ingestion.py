2b4bcb8d9937c99ea0214e55d0f691c7d6830e39
Asmit Pathi <asmitpathi27@gmail.com>
2026-07-12 16:44:57 +0530

    model training and hyperparameters tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 # from src.components.data_transformation import DataTransformation, DataTransformationConfig
+# from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:     # stores configuration values
@@ -48,4 +49,7 @@ class DataIngestion:     # here @dataclass is not used because some functions ar
 #     train_path, test_path= obj.initiate_data_ingestion()
 
 #     data_transformation= DataTransformation()
-#     data_transformation.initiate_data_transformation(train_path, test_path)
+#     train_array, test_array, _= data_transformation.initiate_data_transformation(train_path, test_path)
+
+#     model_trainer= ModelTrainer()
+#     print(model_trainer.initiate_model_trainer(train_array, test_array))
--- src/utils.py
+++ src/utils.py
@@ -7,14 +7,45 @@ import pickle
 
 from src.exception import CustomException
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path, exist_ok= True)    # No error if the directory already exists
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model= list(models.values())[i]
+
+            # hyperparameters tuning
+            # param= params[list(models.keys())[i]]
+            # gs= GridSearchCV(model, param, cv=3)
+            # gs.fit(X_train,y_train)
+            # model.set_params(**gs.best_params_)
+
+            model.fit(X_train, y_train)    # Train model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
